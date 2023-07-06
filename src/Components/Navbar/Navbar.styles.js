530901ec73bf189380831e3710f78ea9d37ef0c1
William Cuc <118580121+WilliamCucJR@users.noreply.github.com>
2023-07-05 19:58:28 -0600

    feat: agregando colores globales

--- src/Components/Navbar/Navbar.styles.js
+++ src/Components/Navbar/Navbar.styles.js
@@ -1,5 +1,6 @@
 import { styled } from "styled-components";
+import { colors } from "../../styles/colors";
 
 export const ContainerNavbar = styled.div`
-    background-color: red;
+    background-color: ${colors.TEXT_GREY_NEUTRAL};
 `;
