7d7a20281d05aeb7e1c2c154bba6b45963c1ddc1
William Cuc <118580121+WilliamCucJR@users.noreply.github.com>
2023-07-06 20:08:43 -0600

    Version final de pagina About Me

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import AboutMe from './Components/AboutMe/AboutMe';
+import Container from './Components/Container/Container';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <>
     <Navbar />
+    <Container>
+      <AboutMe />
+    </Container>
     </>
   );
 }
---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import * as S from './AboutMe.styles';
+import logoGithub from '../../svg/github.svg';
+import logoLinkedin from '../../svg/linkedin.svg';
+import logoUdemy from '../../svg/udemy.svg';
+import logoYoutube from '../../svg/youtube.svg';
+import logoWeb from '../../svg/website.svg';
+
+function AboutMe() {
+
+    const socialNetworks = [
+        {
+            name: "Github",
+            link: "https://google.com",
+            logo: logoGithub
+        },
+        {
+            name: "Linkedin",
+            link: "https://google.com",
+            logo: logoLinkedin
+        },
+        {
+            name: "Udemy",
+            link: "https://google.com",
+            logo: logoUdemy
+        },
+        {
+            name: "Youtube",
+            link: "https://google.com",
+            logo: logoYoutube
+        },
+        {
+            name: "Web",
+            link: "https://google.com",
+            logo: logoWeb
+        },
+    ]
+
+  return (
+    <S.ContainerAboutMe>
+        <S.ContainerFlex>
+            <S.ContainerText>
+                <S.Title>
+                    <p>Hola, yo soy</p>
+                    <p>William Cuc</p>
+                    <p>Programador</p>
+                </S.Title>
+                <S.DescriptionText>
+                    lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsumlorem ipsum lorem ipsumlorem ipsumlorem ipsum lorem ipsumlorem ipsumlorem ipsum lorem ipsumlorem ipsumlorem ipsum lorem ipsumlorem ipsumlorem ipsum lorem ipsumlorem ipsumlorem ipsum lorem ipsumlorem ipsum
+                </S.DescriptionText>
+                <S.ContainerIcons>
+                    {socialNetworks.map((network, index) => (
+                        <a key={index} href={network.link} target='_blank' rel='noreferrer'>
+                            <img src={network.logo} alt={network.name} />
+                        </a>
+                    ))}
+                </S.ContainerIcons>
+            </S.ContainerText>
+            <S.ContainerImage>
+                <img src='/img/profile.png' alt="William Cuc - Programador" />
+            </S.ContainerImage>
+        </S.ContainerFlex>
+    </S.ContainerAboutMe>
+  )
+}
+
+export default AboutMe
