50f8d54f68fa95d41a759fa119be048012794bac
William Cuc <118580121+WilliamCucJR@users.noreply.github.com>
2023-07-06 18:50:04 -0600

    Creacion de navbar y menu amburguesa

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './Navbar.styles';
 
 const Navbar = () => {
+  const [showMenuBurguer, setShowMenuBurguer] = useState(false);
   return (
     <S.ContainerNavbar>
-        Nuestro primer container navbar
+        <S.ContainerLogo>
+          <S.ImageLogo src="/img/logo_dev.png" alt="Logo William Cuc" />
+          <div>
+            <h1>William Cuc</h1>
+            <h2>Programador</h2>
+          </div>
+        </S.ContainerLogo>
+
+        <S.ContainerBurguer onClick={() => setShowMenuBurguer(!showMenuBurguer)}>
+          <S.BurguerMenu />
+          <S.BurguerMenu />
+          <S.BurguerMenu />
+        </S.ContainerBurguer>
+
+
+        <S.ContainerItems showMenuBurguer={showMenuBurguer}>
+          <p>About Me</p>
+          <p>Projects</p>
+          <S.Button>Contact Me</S.Button>
+        </S.ContainerItems>
     </S.ContainerNavbar>
   )
 }
